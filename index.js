e90e896b2e49935c9d36dd21567cb2d7589a1a9b
James Jensen <jnjensen@live.com>
2020-02-05 03:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function takeANumber(line,name){
   line.push(name)
-  var x = line.length + 1
-  return `Welcome, ${name}. You are number ${x} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
