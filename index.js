e7115b0bd2ac4d2e4e4e3030b3e42e70ad4ecfbe
James Jensen <jnjensen@live.com>
2020-02-05 03:45:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(line,name){
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
+
+function nowServing(currentLine){
+  if (currentLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } 
+  else{
+    
+  }
+}
